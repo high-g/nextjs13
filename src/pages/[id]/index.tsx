3824038b30e255cce35e06b908da07b5c1321df8
high-g <altema1217bbfumi@live.jp>
2023-02-13 23:00:41 +0900

    modify: getServerSideProps

---
+++ src/pages/[id]/index.tsx
@@ -0,0 +1,55 @@
+import React from 'react'
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+import { GetServerSideProps } from 'next'
+
+type NewsItem = {
+  id: string
+  title: string
+  content: string
+}
+
+type Props = {
+  news: NewsItem
+}
+
+const Page: React.FC<Props> = ({ news }) => {
+  const router = useRouter()
+  const id = router.query.id
+  return (
+    <div>
+      <h1>{news.title}</h1>
+      <p>{news.content}</p>
+    </div>
+  )
+}
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const queryId = typeof context.query.id === 'string' ? context.query.id : ''
+
+  const dummyList = [
+    {
+      id: '0',
+      title: 'test0',
+      content: 'texttext0',
+    },
+    {
+      id: '1',
+      title: 'test1',
+      content: 'texttext1',
+    },
+    {
+      id: '2',
+      title: 'test2',
+      content: 'texttext2',
+    },
+  ]
+
+  return {
+    props: {
+      news: dummyList[+queryId] || {},
+    },
+  }
+}
+
+export default Page
